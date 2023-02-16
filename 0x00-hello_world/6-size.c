a3251428cd89260f3d7b3e5bcced86380e437225
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-16 23:30:59 +0000

    added 6 size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,11 @@ int main(void)
 	double d;
 	char c;
 
-	printf("Size of an int is $lu", (unsigned long)sizeof(i));
-	printf("Size of a double is $lu", (unsigned long)sizeof(d));
-	printf("Size of a char is $lu", (unsigned long)sizeof(c));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 }

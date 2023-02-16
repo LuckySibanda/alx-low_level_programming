0b0423f57bb446f7718d33f9bfd5ed4f0f946763
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-16 22:38:12 +0000

    added 4 puts c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints a message with proper grammar
+ * and includes a new line character at the end.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	puts("\"Programming is like buliding a multilingual puzzle\n");
-	return 0;
+	return (0);
 }

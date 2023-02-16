7fd6fc0587edc0c92ad05bef4adb010136e80149
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-16 22:34:47 +0000

    added 5 printf c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints a message with proper grammar
+ * and includes a new line character at the end.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return ();
+	return (0);
 }

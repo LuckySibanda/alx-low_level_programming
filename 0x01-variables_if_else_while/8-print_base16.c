93424b77e8933688bead0f08f05a982ee68618bc
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-17 15:13:33 -0800

    prints all numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (letter a 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

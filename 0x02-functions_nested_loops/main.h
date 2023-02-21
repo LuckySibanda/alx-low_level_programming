62ca3cf96f8990821e94a24ff613b36189a294f6
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-21 14:53:07 -0800

    added putchar printer

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,48 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+
+int _putchar(char c)
+
+#endif /* MAIN_H */
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                      6,1           All
+

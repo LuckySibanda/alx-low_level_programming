8c353c6ca71d2734605b03e38d878aa29bbd9dcd
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-22 13:35:24 -0800

    fixed compiling error

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

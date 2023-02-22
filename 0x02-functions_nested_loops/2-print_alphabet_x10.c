bace19a82867101fc182cfac61bdbaab06f678a3
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-22 06:31:58 -0800

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabetx10 - prints times 10 the alphabet.
+ * description - prints 10 times the alphabet, in lowercase.
  *
  */
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 'a'; j <= 'z' j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}

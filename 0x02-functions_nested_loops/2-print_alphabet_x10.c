c8896bea617067ed226e2719d4528d395bb893de
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-22 11:09:27 -0800

    fixed betty description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- * description - prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
  *
+ * Return: x10 a-z
  */
 void print_alphabet_x10(void)
 {
 
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * int _islower(int c) - Entry point
- *
- * Return: Always 0 (Success)
+ * _islower - a function that checks for lowercase character
+ * @c: single letter input
+ * Return: 1 if int c is lowercase, 0 if otherwise
  */
 int _islower(int c);
 {

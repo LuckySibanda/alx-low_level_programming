29a98d7592bd8ddcfe40a441fc6c3efe926f560e
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-27 23:26:12 +0000

    added files that have fixed compiling errors and fixed Betty style coding warnings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ *:w
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len + 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

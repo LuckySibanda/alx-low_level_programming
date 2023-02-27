ac01b0d4f325600a6b13023715424f641e194cf6
Lucky Sibanda <sibandalucky02@gmail.com>
2023-02-27 11:51:11 -0800

    added 0-reset file and readme file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98 using a pointer.
+ * @n: A pointer to the integer to update.
+ *
+ * Return: None.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains C functions around pointers, arrays and strings.

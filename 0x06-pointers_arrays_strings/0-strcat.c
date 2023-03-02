578173cd23834e969896ae1875f10ef321871e4a
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-02 13:26:56 -0800

    fixed output error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int s = 0;
-	int i = 0;
+	int i;
+	int j;
 
-	while (dest[i] != 0)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		s++;
-	}
-	while (src[i] != 0)
-	{
-		dest[s + i] = src[i];
 		i++;
 	}
+	j = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-	dest[s + i] = 0;
+	dest[i] = '\0';
 	return (dest);
 }

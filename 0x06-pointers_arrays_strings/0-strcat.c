36e282df37a35f2841c9fec2d9733110e81c033e
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-02 06:30:14 -0800

    added a function that concatenates twi strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Destination string.
+ * @src: Source string.
+ *
+ * Return: Pointer to resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0, i;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + 1] = src[i];
+
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory is about manipulation of strings using functions in C using pointers and arrays.

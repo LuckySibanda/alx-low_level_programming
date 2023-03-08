c95870e8aed0fd8234cd8ef489b3999db273de89
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-08 07:27:51 -0800

    function that compares two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * wildcmp - functionthat compares two strings and returns
+ * 1 otherwise return 0.
+ * @s1: string source 1
+ * @s2: string source 2
+ * Return: Always 0 (Success)
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}

ebea101ef5869156a2a0977013d19ebd3d5ae545
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-11 05:23:27 -0800

    fixed compiling issue

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - prints the name of the program
@@ -6,9 +8,22 @@
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
-{
-	printf("%s\n", *argv);
 
-	return (0);
+int main(int argc, char *argv[]) {
+    // Get the program name from the command line arguments
+    char *program_name = argv[0];
+    
+    // Find the last occurrence of '/' in the program name
+    char *last_slash = strrchr(program_name, '/');
+    
+    // If there's a slash, the program name is everything after it
+    if (last_slash != NULL) {
+        program_name = last_slash + 1;
+    }
+    
+    // Print the program name, followed by a new line
+    printf("%s\n", program_name);
+    
+    return 0;
 }
+

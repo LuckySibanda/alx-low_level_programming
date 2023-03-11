e545f919db6cb23b5f1216309e2b39a4f75ba1f0
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-11 14:06:38 -0800

    fixed the compile error by changing char to ints and fixing the change system

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, coins;
+	int coins = 0, cents;
 
 	if (argc != 2)
 	{
@@ -19,20 +19,28 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	coins = cents / 25;
-	cents %= 25;
+	cents = atoi(argv[1]);
 
-	coins += cents / 10;
-	cents %= 10;
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-	coins += cents / 5;
-	cents %= 5;
-
-	coins += cents / 2;
-	cents %= 2;
-
-	coins += cents;
-
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
+		coins++;
+	}
 	printf("%d\n", coins);
 
 	return (0);

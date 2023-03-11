40abdee778e066945f498a2b9b20036060e85ee8
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-11 05:41:35 -0800

    added file for offline edititing

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,29 +1,18 @@
-
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - prints the name of the program
- * @argc: argument count
+ * main - prints the name of the program.
+ * @argc: argument counter
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
+int main(int argc, char *argv[])
+{
+	int i;
 
-int main(int argc, char *argv[]) {
-    // Get the program name from the command line arguments
-    char *program_name = argv[0];
-    
-    // Find the last occurrence of '/' in the program name
-    char *last_slash = strrchr(program_name, '/');
-    
-    // If there's a slash, the program name is everything after it
-    if (last_slash != NULL) {
-        program_name = last_slash + 1;
-    }
-    
-    // Print the program name, followed by a new line
-    printf("%s\n", program_name);
-    
-    return 0;
+	for (i = 0; i < *argv; i++)
+	{
+		printf("%s\n", *argv[i]);
+	}
+	Return(0);
 }
-

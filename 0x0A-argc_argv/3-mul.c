1af49fbd569ac0cfc665a33476eda347900874c0
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-11 12:46:12 -0800

    added program that multiplies two nums:
    3
    3
    
    clear
    
    C
    

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the result of a multiplication
+ * @argc: multiplies the integer passed to it
+ * @argv: passed the actual numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+		return (0);
+}

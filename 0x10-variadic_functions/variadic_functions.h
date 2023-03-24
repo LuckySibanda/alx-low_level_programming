9eeda0348202fac3af2fbd83d1b545f6fcf62c80
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-24 16:42:16 -0700

    pushed 2-print_strings.c that akes in three arguments - separator, n, and a variable number of arguments using the ... syntax. It uses va_list, va_arg, and va_start to iterate through the variable arguments.
    The function loops through the number of arguments passed in, and for each argument it retrieves the string using va_arg. If the string is NULL, it prints (nil) instead of the string. If a separator is provided and we are not on the last argument, it prints the separator.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,5 @@
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
-int int_index(int *array, int size, int (*cmp)(int));
 
 #endif

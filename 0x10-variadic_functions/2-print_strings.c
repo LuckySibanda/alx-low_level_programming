61b2ce9eace2df34388d524393c1f80b13796d6b
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-24 16:58:10 -0700

    pushed 2-print_strings function that  takes a variable number of arguments using the stdarg library. It first initializes the argument list with va_start, then iterates over the n arguments using a for loop. Inside the loop, it extracts each argument as a const char* using the va_arg macro. If the argument is NULL, it prints (nil) instead of the string. Otherwise, it prints the string itself using printf.
    After printing each string, the function checks if it should print the separator string by verifying if it's not the last string (i < n - 1) and if the separator is not NULL. If it should print the separator, it prints it using printf.
    
    Finally, the function prints a newline character (\n) to ensure that the output ends with a new line.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,22 +15,28 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i;
-	char *str;
-
+	va_list(args);
 	va_start(args, n);
-	for (i = 0; i < n; i++)
+
+	for (unsigned int i = 0; i < n; i++)
 	{
-		str = va_arg(args, char *);
+		conts char *str = va_arg(args, const char *);
+
 		if (str == NULL)
+		{
 			printf("(nil)");
+		}
 		else
+		{
 			printf("%s", str);
-		if (i != n - 1 && separator != NULL)
+		}
+
+		if (i < n - 1 && separator != NULL)
+		{
 			printf("%s", separator);
+		}
 	}
+	printf("\n");
 
 	va_end(args);
-	printf("\n");
 }
